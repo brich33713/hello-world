1bc98ee9653c4b7b8d5416f3741cadd9c8cb94f6
brich33713 <64274517+brich33713@users.noreply.github.com>
2020-04-25 14:35:34 -0400

    Add files via upload

---
+++ whileloopproblemset.js
@@ -0,0 +1,27 @@
+var count = -10;
+while(count < 20){
+	console.log("this is first" + count);
+	count++;
+}
+
+var second = 10;
+while(second <= 40){
+	console.log("this is second" + second);
+	second+=2;
+}
+
+var third = 301;
+while(third < 334){
+	console.log("this is third" + third);
+	third+=2;
+
+}
+
+var fourth = 5;
+while(fourth < 51){
+	if(fourth % 3 == 0 && fourth % 5 == 0){
+		console.log("this is fourth" + fourth);
+
+	}
+	fourth++;
+}
